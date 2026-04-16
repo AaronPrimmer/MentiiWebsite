ce23d637d21bd5b212ebfda45e5e3664d56a12aa
Aaron Primmer <aaronmprimmer@gmail.com>
2026-04-16 06:57:49 -0400

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MentiiWebsite.Models;
+
+namespace MentiiWebsite.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        //public DbSet<Category> SupermarketCategoriesTbl { get; set; }
+
+        //public DbSet<Product> SupermarketProductsTbl { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MentiiWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
