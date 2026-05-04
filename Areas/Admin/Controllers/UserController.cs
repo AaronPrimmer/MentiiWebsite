c7c3c4ad1beca8af909ccd51a0a5049ad3aca3f0
Aaron Primmer <aaronmprimmer@gmail.com>
2026-05-04 08:45:21 -0400

    Added authorization to admin page and added icons

--- Areas/Admin/Controllers/UserController.cs
+++ Areas/Admin/Controllers/UserController.cs
@@ -1,10 +1,12 @@
 ﻿using MentiiWebsite.Areas.Admin.Models.ViewModels;
 using MentiiWebsite.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MentiiWebsite.Areas.Admin.Controllers
 {
+    [Authorize(Roles = "Admin")]
     [Area("Admin")]
     public class UserController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager) : Controller
     {
